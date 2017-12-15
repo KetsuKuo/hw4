4d07087772d6cd93a20c74775fca568e175fc37a
KetsuKuo <ketsukuo@gmail.com>
2017-12-15 10:28:04 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -15,15 +15,7 @@ window.fbAsyncInit = function() {
       version    : 'v2.11'
     });
     FB.AppEvents.logPageView();
-  };
-
-(function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "https://connect.facebook.net/en_US/sdk.js";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
+  });
 
     $('#startBtn').click(function(e){
       //清空結果
