7221709c88f877987aa056e11facf12985c97bef
KetsuKuo <ketsukuo@gmail.com>
2017-12-15 10:21:03 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,10 +10,20 @@ var $listRoot = $('.page-list');
 // 設定 Facebook AppID
 window.fbAsyncInit = function() {
     FB.init({
-        appId: '1502823243335677', // 若可以，請換成自己的 App ID !
-        xfbml: true,
-        version: 'v2.2'
+      appId      : '1502823243335677',
+      xfbml      : true,
+      version    : 'v2.11'
     });
+    FB.AppEvents.logPageView();
+  };
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "https://connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
 
     $('#startBtn').click(function(e){
       //清空結果
